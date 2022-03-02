b20c061017c60a68596fa67b3b7e8bed20d4302e
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-03-03 00:45:20 +0530

    [Poonam] ADDED: Validating Last Name

--- User Registration Using Regex/UserRegistration.cs
+++ User Registration Using Regex/UserRegistration.cs
@@ -8,6 +8,7 @@ namespace User_Registration_Using_Regex
     class UserRegistration
     {
         public static string NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+        
 
         public bool validateFirstName(String firstName)
         {
