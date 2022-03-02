dd6993b2bd16add18bd4e45bb339882d1de75415
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-03-03 00:10:33 +0530

    Revert "Uc3 validating email"

--- User Registration Using Regex/UserRegistration.cs
+++ User Registration Using Regex/UserRegistration.cs
@@ -8,7 +8,6 @@ namespace User_Registration_Using_Regex
     class UserRegistration
     {
         public static string NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-        public static string EMAIL_PATTERN = "^[0-9a-zA-z]+([._+-][0-9a-zA-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$";
 
         public bool validateFirstName(String firstName)
         {
@@ -17,10 +16,6 @@ namespace User_Registration_Using_Regex
         public bool validateLastName(String lastName)
         {
             return Regex.IsMatch(lastName, NAME_PATTERN);
-        }
-        public bool validateEmail(String email)
-        {
-            return Regex.IsMatch(email, EMAIL_PATTERN);
         }
     }
 }
