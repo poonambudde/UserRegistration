1b9a931381af78a2d266aaeb0366460777a938a6
poonambudde <buddepoonam123@gmail.com>
2022-03-02 23:06:10 +0530

    [Poonam] ADDED: Validating Last Name

--- User Registration Using Regex/Program.cs
+++ User Registration Using Regex/Program.cs
@@ -6,7 +6,17 @@ namespace User_Registration_Using_Regex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to User Registration Programs using Regex");
+
+            UserRegistration userRegistration = new UserRegistration();
+
+            Console.WriteLine("Note: name starts with Cap and has minimun 3 characters");
+            Console.WriteLine("Enter the first name = ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine(userRegistration.validateFirstName(firstName));
+            Console.WriteLine("Enter the last name = ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine(userRegistration.validateLastName(lastName));
         }
     }
 }
---
+++ User Registration Using Regex/UserRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace User_Registration_Using_Regex
+{
+    class UserRegistration
+    {
+        public static string NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+
+        public bool validateFirstName(String firstName)
+        {
+            return Regex.IsMatch(firstName, NAME_PATTERN);
+        }
+        public bool validateLastName(String lastName)
+        {
+            return Regex.IsMatch(lastName, NAME_PATTERN);
+        }
+    }
+}
