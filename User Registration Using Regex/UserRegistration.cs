2a381926f7624eea09679e55f32dc97f8d53806c
poonambudde <buddepoonam123@gmail.com>
2022-03-03 21:37:07 +0530

    [Poonam] ADDED: Validating the password having minimum 8 charactors

--- User Registration Using Regex/UserRegistration.cs
+++ User Registration Using Regex/UserRegistration.cs
@@ -10,21 +10,31 @@ namespace User_Registration_Using_Regex
         public static string NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
         public static string EMAIL_PATTERN = "^[0-9a-zA-z]+([._+-][0-9a-zA-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$";
         public static string MOBILENOFORMAT_PATTERN = "^[0-9]{2}[ ][0-9]{10}$";
+        public static string PASSWORD_PATTERN = "^[0-9a-zA-Z]{8,}";
+
         public bool validateFirstName(String firstName)
         {
             return Regex.IsMatch(firstName, NAME_PATTERN);
         }
+
         public bool validateLastName(String lastName)
         {
             return Regex.IsMatch(lastName, NAME_PATTERN);
         }
+
         public bool validateEmail(String email)
         {
             return Regex.IsMatch(email, EMAIL_PATTERN);
         }
+
         public bool validateMobileNoFormat(String mobileNoFormat)
         {
             return Regex.IsMatch(mobileNoFormat, MOBILENOFORMAT_PATTERN);
+        }
+
+        public bool validatePassword(String password)
+        {
+            return Regex.IsMatch(password, PASSWORD_PATTERN);
         }
     }
 }
