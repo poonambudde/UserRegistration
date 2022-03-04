c606fae18d00ce1053320d3acfc3ae4e0eb66fee
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-03-04 14:16:57 +0530

    [Poonam] ADDED: Validating Email

--- User Registration Using Regex/UserRegistration.cs
+++ User Registration Using Regex/UserRegistration.cs
@@ -7,10 +7,10 @@ namespace User_Registration_Using_Regex
 {
     class UserRegistration
     {
-        public static string NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-        public static string EMAIL_PATTERN = "^[0-9a-zA-z]+([._+-][0-9a-zA-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$";
 
-        public bool validateFirstName(String firstName)
+        public bool validateFirstName(String firs
+        public static string NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+        public static string EMAIL_PATTERN = "^[a-z]{3,}[1-9]{1,4}[@][a-z]{4,}[.][a-z]{3,}$";
         {
             return Regex.IsMatch(firstName, NAME_PATTERN);
         }
