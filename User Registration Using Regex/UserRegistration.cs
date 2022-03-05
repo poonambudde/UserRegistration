488b327e47259d09491d2401d595071a19649bde
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-03-05 22:55:50 +0530

    [Poonam] ADDED: Validating Mobile No

--- User Registration Using Regex/UserRegistration.cs
+++ User Registration Using Regex/UserRegistration.cs
@@ -8,7 +8,7 @@ namespace User_Registration_Using_Regex
     class UserRegistration
     {
         public static string NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-        public static string EMAIL_PATTERN = "^[0-9a-zA-z]+([._+-][0-9a-zA-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$";
+        public static string EMAIL_PATTERN = "^[a-z]{3,}[1-9]{1,4}[@][a-z]{4,}[.][a-z]{3,}$";
         public static string MOBILENOFORMAT_PATTERN = "^[0-9]{2}[ ][0-9]{10}$";
 
         public bool validateFirstName(String firstName)
