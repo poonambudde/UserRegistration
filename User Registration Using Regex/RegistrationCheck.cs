a1d12d53f93b90ff3c2c311be49b84f78ad27e60
poonambudde <buddepoonam123@gmail.com>
2022-03-09 13:01:54 +0530

    [Poonam] ADDED: Ensure All Vaidations using JUnit Test

---
+++ User Registration Using Regex/RegistrationCheck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User_Registration_Using_Regex
+{
+    class RegistrationCheck
+    {
+        private string message;
+        public RegistrationCheck(string message)
+        {
+            this.message = message;
+        }
+        public string checkForFirstName()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Entry is not successful";
+        }
+        public string checkForLastName()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Entry is not successful";
+        }
+        public string checkForEmail()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Entry is not successful";
+        }
+        public string checkForMobile()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Entry is not successful";
+        }
+        public string checkForPassword()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Entry is not successful";
+        }
+    }
+}
